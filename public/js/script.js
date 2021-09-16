d0591d9e697cc0d02c1c7b82bfc0789e2173f98a
Swapnil <swapnil.com>
2021-09-16 23:40:10 +0530

    Chatter video call website first commit

---
+++ public/js/script.js
@@ -0,0 +1,174 @@
+const socket = io("/");
+const videoGrid = document.getElementById('video_grid');
+
+const myPeer = new Peer(undefined, {
+    host: '/',
+    port: '3001'
+});
+
+const user = prompt("Enter your Nmae: ");
+
+const myVideo = document.createElement('video');
+myVideo.muted = true;
+const peers = {}
+
+let myVideoStream;
+navigator.mediaDevices.getUserMedia({
+    video: true,
+    audio: true
+}).then(stream => {
+    myVideoStream = stream;
+    addVideoStream(myVideo, stream);
+
+    myPeer.on('call', (call) => {
+        call.answer(stream);
+        const video = document.createElement('video')
+        call.on('stream', userVideoStream => {
+            addVideoStream(video, userVideoStream);
+        })
+    });
+
+    socket.on('user-connected', (userId) => {
+        // user is joining
+        console.log('New user connected');
+        setTimeout(() => {
+            // user joined
+            connectToNewUser(userId, stream)
+        }, 1000)
+    })
+});
+
+socket.on('user-disconnected', (userId) => {
+    if (peers[userId]) peers[userId].close()
+})
+
+
+myPeer.on('open', (id) => {
+    socket.emit("join-room", ROOM_ID, id, user);
+})
+
+function connectToNewUser(userId, stream) {
+    const call = myPeer.call(userId, stream)
+    const video = document.createElement('video');
+    call.on('stream', (userVideoStream) => {
+        addVideoStream(video, userVideoStream)
+    })
+    call.on('close', () => {
+        video.remove()
+    })
+
+    peers[userId] = call
+}
+
+function addVideoStream(video, stream) {
+    video.srcObject = stream;
+    video.addEventListener('loadedmetadata', () => {
+        video.play()
+    })
+    videoGrid.append(video)
+}
+
+
+//////////////  Input Link  /////////////////////
+function getInputLink() {
+    const Link = document.getElementById("input_link").value;
+    const code = Link.split('/');
+    window.open(code[3]);
+}
+
+
+////////////// URL Copy To Clipboard  //////////////
+document.getElementById("invite_btn").addEventListener("click", getURL);
+
+function getURL() {
+    const c_url = window.location.href;
+    copyToClipboard(c_url);
+    alert("Url Copied to Clipboard,\nShare it with your Friends!\nUrl: " + c_url);
+}
+
+function copyToClipboard(text) {
+    var dummy = document.createElement("textarea");
+    document.body.appendChild(dummy);
+    dummy.value = text;
+    dummy.select();
+    document.execCommand("copy");
+    document.body.removeChild(dummy);
+}
+
+////////////// MUTE UNMUTE VIDEO / AUDIO ///////////////
+const mute_btn = document.querySelector('#mute_btn');
+const video_btn = document.querySelector('#video_btn');
+
+mute_btn.addEventListener('click', () => {
+    const enabled = myVideoStream.getAudioTracks()[0].enabled;
+
+    if (enabled) {
+        myVideoStream.getAudioTracks()[0].enabled = false;
+        html = `<i class="fas fa-microphone-alt-slash"></i>`;
+        // mute_btn.classList.toggle("background__red");
+        mute_btn.innerHTML = html;
+    }
+    else {
+        myVideoStream.getAudioTracks()[0].enabled = true;
+        html = `<i class="fas fa-microphone-alt"></i>`;
+        // mute_btn.classList.toggle("background__red");
+        mute_btn.innerHTML = html;
+    }
+})
+
+
+video_btn.addEventListener("click", () => {
+    const enabled = myVideoStream.getVideoTracks()[0].enabled;
+    if (enabled) {
+        myVideoStream.getVideoTracks()[0].enabled = false;
+        html = `<i class="fas fa-video-slash"></i>`;
+        //   video_btn.classList.toggle("background__red");
+        video_btn.innerHTML = html;
+    } else {
+        myVideoStream.getVideoTracks()[0].enabled = true;
+        html = `<i class="fas fa-video"></i>`;
+        //   video_btn.classList.toggle("background__red");
+        video_btn.innerHTML = html;
+    }
+});
+
+
+
+/////////////////   End Call  ///////////////////////
+document.getElementById("end_btn").addEventListener("click", endCall);
+
+function endCall() {
+    window.location.href = "/";
+}
+
+
+//////////////////  CHAT SECTION  /////////////////////
+
+let text = document.querySelector("#chat_message");
+let send = document.getElementById("send_message");
+let messages = document.querySelector(".messages");
+
+send.addEventListener("click", (e) => {
+    if (text.value.length !== 0) {
+        socket.emit("message", text.value);
+        text.value = "";
+    }
+});
+
+text.addEventListener("keydown", (e) => {
+    if (e.key === "Enter" && text.value.length !== 0) {
+        socket.emit("message", text.value);
+        text.value = "";
+    }
+});
+
+
+socket.on("createMessage", (message, user_name) => {
+    messages.innerHTML =
+        messages.innerHTML +
+        `<div class="message">
+          <b><i class="far fa-user-circle"></i> <span> ${user_name === name ? "me" : user_name
+        } : </span> </b>
+          <span>${message}</span>
+      </div>`;
+});
---
+++ server.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const app = express();
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
+// const path = require('path');
+const { v4: uuidV4 } = require('uuid');
+// const hbs = require('hbs');
+const PORT = process.env.PORT || 3000;
+
+app.set("view engine", "ejs");
+
+// app.set('view engine', 'hbs');
+app.use(express.static('public'));
+// hbs.registerPartials(__dirname + '/views/partials')
+
+
+
+app.get('/', (req, res) => {
+    res.render('index');
+})
+
+app.get('/create_room/', (req, res) => {
+    res.redirect(`/${uuidV4()}`);
+})
+
+app.get('/:room', (req, res) => {
+    const name = "swapnil";
+    res.render('room', { roomId: req.params.room });
+});
+
+io.on("connection", (socket) => {
+    socket.on("join-room", (roomId, userId, user_name) => {
+        console.log(roomId, userId);
+        socket.join(roomId);
+        socket.broadcast.to(roomId).emit("user-connected", userId);
+
+        socket.on("disconnect", () => {
+            socket.broadcast.to(roomId).emit("user-disconnected", userId);
+          });
+
+        socket.on("message", (message) => {
+            io.to(roomId).emit("createMessage", message, user_name);
+        })
+    })
+})
+
+
+server.listen(PORT, (res, req) => console.log(`connected to port ${PORT}`));
